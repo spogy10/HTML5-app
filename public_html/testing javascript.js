63761ef329ce556190e4473046f729cc98fad962
Paul <poliverjr@hotmail.com>
2016-03-13 15:44:47 -0500

    comitted

---
+++ public_html/testing javascript.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var num1 = 1;
+function add(add1)
+{
+    return add1 + 2;
+}
+//alert(add(n
